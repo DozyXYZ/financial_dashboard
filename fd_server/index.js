f593957e6614ec2a35fc339b34abef1ec9d96396
Dozy <dotuan14747@gmail.com>
2025-02-07 18:31:44 +0200

    Connect to MongoDB, set up .env, .gitignore. Create mock data

--- fd_server/index.js
+++ fd_server/index.js
@@ -17,5 +17,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-console.log("Hello World");
 /* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(async () => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+    })
+    .catch((error) => console.log(`${error} did not connect`));
