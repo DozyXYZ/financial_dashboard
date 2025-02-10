cdc340155870cb9d4a8f663799301ca76d01cb61
Dozy <dotuan14747@gmail.com>
2025-02-10 17:09:07 +0200

    Configured endpoints for products. Added data to MongoDb. Created models/Transaction.js Product.js. Created routes/product.js. Modified index.js

--- fd_server/index.js
+++ fd_server/index.js
@@ -6,8 +6,11 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import kpiRoutes from './routes/kpi.js';
+import productRoutes from './routes/product.js';
 import KPI from './models/KPI.js';
+import Product from './models/Product.js';
 import { kpis } from './data/data.js';
+import { products } from './data/data.js';
 
 /* CONFIGURATION */
 dotenv.config();
@@ -34,5 +37,6 @@ mongoose
         /* ADD DATA ONE TIME ONLY OR AS NEEDED */
         // await mongoose.connection.db.dropDatabase();
         // KPI.insertMany(kpis);
+        // Product.insertMany(products);
     })
     .catch((error) => console.log(`${error} did not connect`));
